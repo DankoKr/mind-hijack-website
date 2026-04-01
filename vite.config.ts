7b4378d8bc0a0f96eb1e31e57e173167bb680eb2
DankoKr <dankokralski776@gmail.com>
2026-04-01 09:00:31 +0300

    adjust: vite.config base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/mind-hijack/",
+  base: "/mind-hijack-website/",
   server: {
     host: "::",
     port: 8080,
